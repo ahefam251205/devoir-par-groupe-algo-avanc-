6f64b9559c1f65ec72af9207f6c0f9c96f388128
ahefam251205 <tiavinamahefa4@gmail.com>
2025-09-24 13:56:28 +0200

    Ajout du fichier abr_structure.py

---
+++ abr_structure.py
@@ -0,0 +1,15 @@
+# Partie 1 - Début du projet : définition de la structure de l'arbre
+
+class Noeud:
+    def __init__(self, valeur):
+        self.valeur = valeur
+        self.gauche = None
+        self.droite = None
+
+class ABR:
+    def __init__(self):
+        self.racine = None
+
+    def inserer(self, valeur):
+        # fonction vide pour l'instant, sera complétée par le prochain membre
+        pass
